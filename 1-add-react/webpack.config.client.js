3daf46dc935e2c6d63f10190bd9f8f922843e931
Carey Hinoki <carey.r.hinoki.-nd@disney.com>
2016-01-12 17:45:23 -0800

    feat(*): Add webpack build.

---
+++ 1-add-react/webpack.config.client.js
@@ -0,0 +1,52 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+    bundle: [
+      './src/client'
+    ],
+
+    index: './src/index.html'
+  },
+
+  output: {
+    filename: '[name].js',
+    path:     path.join(__dirname, 'client')
+  },
+
+  module: {
+    loaders: [{
+      // https://github.com/webpack/file-loader
+      exclude: /node_modules/,
+      loader:  'file?name=[name].[ext]',
+      test:    /\.html$/
+    }],
+
+    postLoaders: [{
+      // https://github.com/babel/babel-loader
+      exclude: /node_modules/,
+      loader:  'babel-loader',
+      test:    /\.jsx?/,
+
+      query: {
+        presets: [
+          'es2015',
+          'react'
+        ]
+      }
+    }]
+  },
+
+  // https://webpack.github.io/docs/configuration.html#resolve
+  resolve: {
+    // https://webpack.github.io/docs/configuration.html#resolve-extensions
+    extensions: [
+      '',
+      '.js',
+      '.jsx'
+    ],
+
+    // https://webpack.github.io/docs/configuration.html#resolve-root
+    root: path.join(__dirname, 'src')
+  }
+};
