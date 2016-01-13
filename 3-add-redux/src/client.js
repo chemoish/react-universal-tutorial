1d450161acf92e60b8f20daec96773937dd5eaf7
Carey Hinoki <carey.r.hinoki.-nd@disney.com>
2016-01-13 13:13:10 -0800

    feat(redux-simple-router): Add.

--- 3-add-redux/src/client.js
+++ 3-add-redux/src/client.js
@@ -6,12 +6,15 @@ import { createHistory } from 'history';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { Router } from 'react-router';
+import { syncReduxAndRouter } from 'redux-simple-router';
 
 import configureStore from './store/configure-store';
 import routes from './route';
 
 const history = createHistory();
 const store = configureStore();
+
+syncReduxAndRouter(history, store);
 
 const component = (
   <Provider store={store} key="provider">
